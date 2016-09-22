d371f90dcb0a057915c6a90f102f8b8f62384c08
Athul Raj <athulbraj@gmail.com>
2016-09-22 17:48:55 +0530

    Server

--- prun.js
+++ prun.js
@@ -1,11 +1,34 @@
-var page = require('webpage').create(),
-	server = 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket/form2',
-	data = 'regno1=12130024&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
+var pdf = require('html-pdf');
+var request = require('request');
 
-page.open(server, 'POST', data, function(status) {
-	console.log("Status: " + status);
-	if(status === "success") {
-	}
-	page.render('example.pdf');
-	phantom.exit();
-});
+/*
+*	fetchPhoto?regno1=12130024
+*	fetchSign?regno1=12130024
+*/
+
+var options = {
+	format: 'A2',
+	base: "http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket"
+};
+
+request({
+		url : 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket/form2',
+		method: 'POST',
+		form: {
+			regno1:12130024,
+			deg_name:'B.Tech',
+			semister:8,
+			month:'September',
+			year:2016,
+			reg_type:'Supply/Improvement',
+			checktype:'check'
+    	}
+		},function (error, response, body) {
+    		if (!error && response.statusCode == 200) {
+    			pdf.create(body, options).toFile('./businesscard.pdf', function(err, res) {
+					if (err) return console.log(err);
+		  			console.log(res);
+				});
+    		}
+    	}
+);
--- run.js
+++ run.js
@@ -1,35 +1,11 @@
-var pdf = require('html-pdf');
-var request = require('request');
+var page = require('webpage').create(),
+	server = 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket/form2',
+	data = 'regno1='+system.args[1]+'&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
 
-/*
-*	fetchPhoto?regno1=12130024
-*	fetchSign?regno1=12130024
-*/
-
-var options = {
-	format: 'A2',
-	base: "http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket"
-};
-
-request({
-		url : 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket/form2',
-		method: 'POST',
-		form: {
-			regno1:12130024,
-			deg_name:'B.Tech',
-			semister:8,
-			month:'September',
-			year:2016,
-			reg_type:'Supply/Improvement',
-			checktype:'check'
-    	}
-		},function (error, response, body) {
-    		if (!error && response.statusCode == 200) {
-    			pdf.create(body, options).toFile('./businesscard.pdf', function(err, res) {
-					if (err) return console.log(err);
-		  			console.log(res);
-				});
-    		}
-    	}
-);
-
+page.open(server, 'POST', data, function(status) {
+	console.log("Status: " + status);
+	if(status === "success") {
+	}
+	page.render('results/'system.args[1]'.pdf');
+	phantom.exit();
+});
