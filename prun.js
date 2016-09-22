513690be708f699048ecba9bdf0e94588074ec99
Athul Raj <athulbraj@gmail.com>
2016-09-22 17:06:57 +0530

    Server

--- prun.js
+++ prun.js
@@ -1,6 +1,6 @@
 var page = require('webpage').create(),
 	server = 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket',
-	data = 'regno1:12130024&deg_name:B.Tech&semister:8&month:September&year:2016&reg_type:Supply/Improvement&checktype:check';
+	data = 'regno1=12130024&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
 page.open(server, 'post', data, function(status) {
 	console.log("Status: " + status);
 	if(status === "success") {
