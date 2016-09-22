6c59e493f522ac7713098c05e0bea1678522c2d2
Athul Raj <athulbraj@gmail.com>
2016-09-22 17:14:42 +0530

    Server

--- prun.js
+++ prun.js
@@ -1,17 +1,53 @@
 var page = require('webpage').create(),
 	server = 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket',
 	data = 'regno1=12130024&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
-
-page.onError = function(msg, trace) {
-  var msgStack = ['PHANTOM ERROR: ' + msg];
-  if (trace && trace.length) {
-    msgStack.push('TRACE:');
-    trace.forEach(function(t) {
-      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+page.onResourceRequested = function (request) {
+    system.stderr.writeLine('= onResourceRequested()');
+    system.stderr.writeLine('  request: ' + JSON.stringify(request, undefined, 4));
+};
+ 
+page.onResourceReceived = function(response) {
+    system.stderr.writeLine('= onResourceReceived()' );
+    system.stderr.writeLine('  id: ' + response.id + ', stage: "' + response.stage + '", response: ' + JSON.stringify(response));
+};
+ 
+page.onLoadStarted = function() {
+    system.stderr.writeLine('= onLoadStarted()');
+    var currentUrl = page.evaluate(function() {
+        return window.location.href;
     });
-  }
-  console.error(msgStack.join('\n'));
-  phantom.exit(1);
+    system.stderr.writeLine('  leaving url: ' + currentUrl);
+};
+ 
+page.onLoadFinished = function(status) {
+    system.stderr.writeLine('= onLoadFinished()');
+    system.stderr.writeLine('  status: ' + status);
+};
+ 
+page.onNavigationRequested = function(url, type, willNavigate, main) {
+    system.stderr.writeLine('= onNavigationRequested');
+    system.stderr.writeLine('  destination_url: ' + url);
+    system.stderr.writeLine('  type (cause): ' + type);
+    system.stderr.writeLine('  will navigate: ' + willNavigate);
+    system.stderr.writeLine('  from page\'s main frame: ' + main);
+};
+ 
+page.onResourceError = function(resourceError) {
+    system.stderr.writeLine('= onResourceError()');
+    system.stderr.writeLine('  - unable to load url: "' + resourceError.url + '"');
+    system.stderr.writeLine('  - error code: ' + resourceError.errorCode + ', description: ' + resourceError.errorString );
+};
+ 
+page.onError = function(msg, trace) {
+    system.stderr.writeLine('= onError()');
+    var msgStack = ['  ERROR: ' + msg];
+    if (trace) {
+        msgStack.push('  TRACE:');
+        trace.forEach(function(t) {
+            msgStack.push('    -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function + '")' : ''));
+        });
+    }
+    system.stderr.writeLine(msgStack.join('\n'));
 };
 page.open(server, 'post', data, function(status) {
 	console.log("Status: " + status);
