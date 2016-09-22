d8128cb7a85b157d1d4f371f7bd4c898fe9ed7e3
Athul Raj <athulbraj@gmail.com>
2016-09-22 10:35:54 +0530

    Server

--- run.js
+++ run.js
@@ -2,7 +2,16 @@ var fs = require('fs');
 var pdf = require('html-pdf');
 var request = require('request');
 var options = { format: 'Letter' };
-pdf.create(request('http://google.com/doodle.png'), options).toFile('./businesscard.pdf', function(err, res) {
+var html;
+
+request('http://google.com/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body); // Show the HTML for the Modulus homepage.
+        html = body;
+    }
+});
+
+pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
   if (err) return console.log(err);
-  console.log(res); // { filename: '/app/businesscard.pdf' }
+  console.log(res);
 });
