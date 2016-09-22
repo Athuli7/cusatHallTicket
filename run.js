d644bc318f60f628832a40ac100bf56d74072661
Athul Raj <athulbraj@gmail.com>
2016-09-22 10:32:57 +0530

    Server

--- run.js
+++ run.js
@@ -1,9 +1,8 @@
 var fs = require('fs');
 var pdf = require('html-pdf');
 var request = require('request');
-var html = request('http://google.com/doodle.png');
 var options = { format: 'Letter' };
-pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+pdf.create(request('http://google.com/doodle.png'), options).toFile('./businesscard.pdf', function(err, res) {
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
 });
