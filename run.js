ec985fcb5d5c09212590724d96d4f3cb91fbb100
Athul Raj <athulbraj@gmail.com>
2016-09-22 18:07:35 +0530

    Server

--- run.js
+++ run.js
@@ -1,12 +1,18 @@
 var system = require('system');
 var page = require('webpage').create(),
 	server = 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket/form2',
-	data = 'regno1='+system.args[1]+'&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
-
+	data =		'regno1='		+ system.args[1] +
+				'&deg_name='	+ system.args[2] +
+				'&semister='	+ system.args[3] +
+				'&month='		+ system.args[4] +
+				'&year='		+ system.args[5] +
+				'&reg_type='	+ system.args[6] +
+				'&checktype='	+ system.args[7],
+	folder = system.args[3] + system.args[6] + system.args[4] + system.args[5];
 page.open(server, 'POST', data, function(status) {
 	console.log("Status: " + status);
 	if(status === "success") {
 	}
-	page.render('results/'+system.args[1]+'.pdf');
+	page.render('results/'+ folder +'/'+system.args[1]+'.pdf');
 	phantom.exit();
 });
