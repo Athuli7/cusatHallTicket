65b3380dd755714500fcf2782215bf12e91c2469
Athul Raj <athulbraj@gmail.com>
2016-09-22 17:52:56 +0530

    Server

--- run.js
+++ run.js
@@ -1,12 +1,11 @@
-console.log(system.args[2]);
 var page = require('webpage').create(),
 	server = 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket/form2',
-	data = 'regno1='+system.args[2]+'&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
+	data = 'regno1=12130024&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
 
 page.open(server, 'POST', data, function(status) {
 	console.log("Status: " + status);
 	if(status === "success") {
 	}
-	page.render('results/'system.args[2]'.pdf');
+	page.render('results/12130024.pdf');
 	phantom.exit();
 });
