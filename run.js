b5c842f51a9be9b110437d760f11a3ee8b19a2c2
Athul Raj <athulbraj@gmail.com>
2016-09-22 17:50:34 +0530

    Server

--- run.js
+++ run.js
@@ -1,3 +1,4 @@
+console.log(system.args[1]);
 var page = require('webpage').create(),
 	server = 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket/form2',
 	data = 'regno1='+system.args[1]+'&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
