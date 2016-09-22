e150f985782397e0dcfb712ba194da886c1a2660
Athul Raj <athulbraj@gmail.com>
2016-09-22 17:14:05 +0530

    Server

--- prun.js
+++ prun.js
@@ -2,7 +2,7 @@ var page = require('webpage').create(),
 	server = 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket',
 	data = 'regno1=12130024&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
 
-phantom.onError = function(msg, trace) {
+page.onError = function(msg, trace) {
   var msgStack = ['PHANTOM ERROR: ' + msg];
   if (trace && trace.length) {
     msgStack.push('TRACE:');
