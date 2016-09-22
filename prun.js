b3d55f384f5c705b044d799df77e944fa48d792c
Athul Raj <athulbraj@gmail.com>
2016-09-22 17:10:23 +0530

    Server

--- prun.js
+++ prun.js
@@ -1,6 +1,12 @@
 var page = require('webpage').create(),
 	server = 'http://exam.cusat.ac.in/erp5/cusat/Cusat-Hall-Ticket',
 	data = 'regno1=12130024&deg_name=B.Tech&semister=8&month=September&year=2016&reg_type=Supply/Improvement&checktype=check';
+page.onError = function (msg, trace) {
+    console.log(msg);
+    trace.forEach(function(item) {
+        console.log('  ', item.file, ':', item.line);
+    });
+};
 page.open(server, 'post', data, function(status) {
 	console.log("Status: " + status);
 	if(status === "success") {
@@ -8,9 +14,3 @@ page.open(server, 'post', data, function(status) {
 	page.render('example.pdf');
 	phantom.exit();
 });
-page.onError = function (msg, trace) {
-    console.log(msg);
-    trace.forEach(function(item) {
-        console.log('  ', item.file, ':', item.line);
-    });
-};
