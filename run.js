a0935752f702c85c3f4a50651df64e09ce55cad8
Athul Raj <athulbraj@gmail.com>
2016-09-22 10:42:23 +0530

    Server

--- run.js
+++ run.js
@@ -5,7 +5,7 @@ var options = { format: 'Letter' };
 
 request('http://google.com/', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-    	pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
+    	pdf.create(body, options).toFile('./businesscard.pdf', function(err, res) {
 			if (err) return console.log(err);
 		  		console.log(res);
 		});
