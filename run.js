0082389024fec2a17ba9e8fdffa382bc74029905
Athul Raj <athulbraj@gmail.com>
2016-09-22 15:56:36 +0530

    Server

--- run.js
+++ run.js
@@ -1,14 +1,29 @@
-var fs = require('fs');
 var pdf = require('html-pdf');
 var request = require('request');
-var options = { format: 'Letter' };
 
-request('http://google.com/', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-    	pdf.create(body, options).toFile('./businesscard.pdf', function(err, res) {
-			if (err) return console.log(err);
-		  		console.log(res);
-		});
-    }
-});
+var options = {
+	format: 'A4'
+};
+
+request({
+		url : 'http://google.com/',
+		method: 'POST',
+		form: {
+			regno1:12130024,
+			deg_name:'B.Tech',
+			semister:8,
+			month:'September',
+			year:2016,
+			reg_type:'Supply/Improvement',
+			checktype:'check'
+    	}
+		},function (error, response, body) {
+    		if (!error && response.statusCode == 200) {
+    			pdf.create(body, options).toFile('./businesscard.pdf', function(err, res) {
+					if (err) return console.log(err);
+		  			console.log(res);
+				});
+    		}
+    	}
+);
 
