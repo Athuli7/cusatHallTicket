d26edce802e3215007dde75cbbd72f0c59765f65
Athul Raj <athulbraj@gmail.com>
2016-09-22 10:36:41 +0530

    Server

--- run.js
+++ run.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var pdf = require('html-pdf');
 var request = require('request');
 var options = { format: 'Letter' };
-var html;
+var html = "Hello";
 
 request('http://google.com/', function (error, response, body) {
     if (!error && response.statusCode == 200) {
